8ed8c1aadce4f42dfa346f12014a20f5d58ac6be
Marc Nopprapun <mnopprapun@gmail.com>
2021-04-05 16:57:26 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400){ return `This one is on me!`}
+  if (feet <= 2499){return `I will gladly take your thirty bucks.`}
+if (feet > 2500){return `No can do.`}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+ if (city === 'NYC'){return `Ok, sounds good.`}
+ else if (city !== 'NYC'){return `No go.`}
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  // Write your code here! 
+  if (tip === `generous`)
+  {return `Thank you so much.`}
+if (tip === `not as generous`)
+  {return `Thank you.`}
+else 
+  {return "Bye."}
 }
